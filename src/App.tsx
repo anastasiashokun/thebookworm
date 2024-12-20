28abdf298ebe564532554c7953ee7e41d848252e
anastasiashokun <nastyashokun@gmail.com>
2024-12-20 03:30:37 +0200

    adaptation for mobile phone

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import {  RouterProvider } from 'react-router-dom'
-import useRouter from './hooks/router/useRouter'
-
+import { RouterProvider } from 'react-router-dom';
+import useRouter from './hooks/router/useRouter';
 import { createGlobalStyle } from 'styled-components';
+import { useState, useEffect } from 'react';
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+};
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap');
@@ -12,14 +33,19 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
-    
-  const Router = useRouter()
+  const Router = useRouter();
+  const size = useWindowSize();
+
+  useEffect(() => {
+    console.log(`Width: ${size.width}, Height: ${size.height}`);
+  }, [size]);
+
   return (
     <>    
-    <GlobalStyle/>
-     <RouterProvider router={Router}/> 
+      <GlobalStyle />
+      <RouterProvider router={Router}/> 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
